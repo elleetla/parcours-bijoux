13402ec9dde8345914d1ef33f9dd2c149a6bd0fb
elleetla <elleetla@gmail.com>
2017-05-11 12:25:43 +0200

    in categories pages(displaying all events), remove title and descriptions of the page

--- wp-content/themes/genesis-sample/taxonomy.php
+++ wp-content/themes/genesis-sample/taxonomy.php
@@ -6,8 +6,13 @@
  * Time: 13:10
  */
 
+//* Display picture of the slider and its title (/!\ display only slider of the 1st post)
 add_action( 'genesis_after_header', 'slider_project', 10 );
 
+//* Removes Title and Description on Archive, Taxonomy, Category, Tag
+remove_action( 'genesis_before_loop', 'genesis_do_taxonomy_title_description', 15 );
+
+//* Remove all post content
 remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
 
 genesis();
